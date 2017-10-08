8bd6f13ef556402d2c2178997b65a275b62f9e3d
Nguyen Hoai Nam <namptit307@gmail.com>
2017-10-09 00:22:19 +0700

    Update

---
+++ config.py
@@ -0,0 +1,4 @@
+IP_Keystone = '192.168.10.103'
+username = 'admin'
+password = 'abc123'
+project_name = 'admin'
--- run.py
+++ run.py
@@ -3,22 +3,25 @@
 
 import optparse
 
+import config
+
 
 def main():
     parser = optparse.OptionParser(usage="usage: %prog [options]",
                                    version="%prog 1.0")
     parser.add_option('-u', '--username', action='store', type='string',
-                      dest='username', default='admin', metavar='USERNAME',
+                      dest='username', default=config.username,
+                      metavar='USERNAME',
                       help='The name of users')
     parser.add_option('-p', '--password', action='store', type='string',
-                      dest='password', default='admin', metavar='PASSWORD',
+                      dest='password', default=config.password, metavar='PASSWORD',
                       help='The password of users')
     parser.add_option('-n', '--project_name', action='store', type='string',
-                      dest='project_name', default='admin',
+                      dest='project_name', default=config.project_name,
                       metavar='PROJECT_NAME',
                       help='The project name of users')
     parser.add_option('-i', '--keystone', action='store', type='string',
-                      dest='keystone_ip', default=None,
+                      dest='keystone_ip', default=config.IP_Keystone,
                       metavar='KEYSTONE IP',
                       help='The IP of keystone')
     (options, args) = parser.parse_args()
